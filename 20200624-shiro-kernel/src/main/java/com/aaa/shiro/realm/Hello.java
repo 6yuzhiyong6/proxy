13d58e2dfca9115f55b3552bdfad8589717483cb
ybamboo <59681924+ybamboo@users.noreply.github.com>
2020-06-26 17:56:48 +0800

    这是第一次使用

---
+++ 20200624-shiro-kernel/src/main/java/com/aaa/shiro/realm/Hello.java
@@ -0,0 +1,14 @@
+package com.aaa.shiro.realm;
+
+/**
+ * @Author: Mr.Yu
+ * @Date: 2020/6/26 17:56
+ * @description
+ * @Version 1.0
+ */
+public class Hello {
+    private void  test(){
+        System.out.println("你好！");
+    }
+}
+
